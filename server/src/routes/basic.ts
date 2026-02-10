c3e215af2ee6ca58cd3869fbcdde7cce85666633
antonovmike <antonovma@outlook.com>
2026-02-10 21:16:05 +0100

    [IMP] Add try/catch to router "/tele

--- server/src/routes/basic.ts
+++ server/src/routes/basic.ts
@@ -5,8 +5,16 @@ import tele_server from "../services/telegram_scraper";
 const router = express.Router();
 
 // GET / tele_server
-router.get("/tele", (_req: Request, res: Response) => {
-  tele_server.emit("request", _req, res);
+router.get("/tele", async (_req: Request, res: Response) => {
+  try {
+    const messages = await new Promise((_resolve, _reject) => {
+      tele_server.emit("request", _req, res);
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    const err = error as Error;
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // GET /hello
