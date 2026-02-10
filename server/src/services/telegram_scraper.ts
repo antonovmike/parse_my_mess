e88c83b295cdd9a8a0d68cc7d15533c0f44ebf3c
antonovmike <antonovma@outlook.com>
2026-02-10 20:14:03 +0100

    [IMP] Improved messages formatting
    - "Content-Type" changed from "application/json" to "text/plain"
    - Added function formattedMessages
    - res.end now calls formattedMessages instead of JSON.stringify

--- server/src/services/telegram_scraper.ts
+++ server/src/services/telegram_scraper.ts
@@ -3,13 +3,21 @@ import http from "http";
 import { parseTelegramChannel } from "./html-parser";
 import { TELEGRAM_CHANNEL } from "../serverConfig.js";
 
+function formattedMessages(
+  messages: { message_text: string; datetime: string }[],
+): string {
+  return messages
+    .map((msg) => `${msg.message_text}\n${msg.datetime}`)
+    .join("\n\n");
+}
+
 const tele_server = http.createServer(async (_req, res) => {
   try {
     const messages = await parseTelegramChannel(TELEGRAM_CHANNEL as string);
     res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(JSON.stringify(messages));
+    res.end(formattedMessages(messages));
   } catch (error) {
     res.statusCode = 500;
     res.end(JSON.stringify({ error: "Failed to retrieve messages." }));
