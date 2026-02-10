61d147d71c707c4b604c4aa5ac7f0c6eac117f63
antonovmike <antonovma@outlook.com>
2026-02-10 19:13:22 +0100

    [REM] Remove unnecessary console.log

--- server/src/serverConfig.ts
+++ server/src/serverConfig.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log("server/src/serverConfig.ts\n" + process.env.TELEGRAM_CHANNEL_NAME);
 
 export const TELEGRAM_CHANNEL = process.env.TELEGRAM_CHANNEL_NAME;
