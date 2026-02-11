53f13d4b286f1826ed817b1bac80add322e688bf
antonovmike <antonovma@outlook.com>
2026-02-11 10:20:40 +0100

    [ADD] Add minimal React Vite frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Parse my mess</h1>
+      <div className="card">
+        <p>An application to parse Telegram open channel messages</p>
+      </div>
+      <p className="read-the-docs">Demo version</p>
+    </>
+  );
+}
+
+export default App;
