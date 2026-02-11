e98f17cbb7779881b1cce9fa5a61f9c7cfbcd042
antonovmike <antonovma@outlook.com>
2026-02-11 15:39:57 +0100

    [ADD] Refactor interface TelegramMessage
    - Created file server/src/types/telegram.ts
    - Moved interface TelegramMessage from html-parser.ts to telegram.ts

--- server/src/services/html-parser.ts
+++ server/src/services/html-parser.ts
@@ -1,15 +1,12 @@
 import { telegram_scraper } from "telegram-scraper";
 
-interface TelegramMessage {
-  message_text: string;
-  datetime: string;
-}
+import { TelegramMessage } from "../types/telegram";
 
 export async function parseTelegramChannel(
   channel: string,
 ): Promise<TelegramMessage[]> {
   const result = await telegram_scraper(channel);
-  const messages = JSON.parse(result);
+  const messages: TelegramMessage[] = JSON.parse(result);
 
   return messages.map((msg: any) => ({
     message_text: msg.message_text,
