c583be412867b7facbdd795113410e02f1f484b2
antonovmike <antonovma@outlook.com>
2026-02-11 08:40:39 +0100

    [IMP] Enable CORS for specified origin and credentials to connect frontend and backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 import basicRoutes from "./routes/basic.js";
 import telegramRoutes from "./routes/telegramRouts.js";
@@ -6,7 +8,12 @@ import telegramRoutes from "./routes/telegramRouts.js";
 import logger from "./middleware/logger.js";
 import { errorHandler, notFound } from "./middleware/errorHandler.js";
 
+dotenv.config();
+
 const app = express();
+
+// Enable CORS for specified origin and credentials to connect frontend and backend
+app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
 
 // Middleware logging each request
 app.use(logger);
